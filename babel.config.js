90583278cfb7edf484937e9ac9c702110d406961
Danny Hieber <dwhieb@gmail.com>
2019-03-15 20:29:18 -0700

    setup Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,19 @@
-module.exports = {};
+module.exports = {
+  presets: [
+    [
+      `@babel/preset-env`,
+      {
+        targets: {
+          browsers: [
+            `last 2 Chrome major versions`,
+            `last 2 Edge major versions`,
+            `last 2 Firefox major versions`,
+            `last 2 iOS major versions`,
+            `last 2 Safari major versions`,
+            `maintained node versions`,
+          ],
+        },
+      },
+    ],
+  ],
+};
