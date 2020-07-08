ec36df75f62e129289ac28da56872f0337e7370e
Danny Hieber <dwhieb@gmail.com>
2020-07-07 22:51:26 -0500

    Nuuchahnulth tests passing

--- test/data/data.test.js
+++ test/data/data.test.js
@@ -1,15 +1,31 @@
-import convert from '../../src/index.js';
-import expect  from 'expect.js';
+import convert           from '../../src/index.js';
+import expect            from 'expect.js';
+import { fileURLToPath } from 'url';
+import path              from 'path';
+import { promises }      from 'fs';
+
+const currentDir   = path.dirname(fileURLToPath(import.meta.url));
+const { readFile } = promises;
 
 describe(`data`, () => {
 
-  it(`parses Chitimacha`, () => {
-    const test = () => convert(data.Chitimacha);
+  it(`parses Chitimacha`, async () => {
+    const Chitimacha = await readFile(path.join(currentDir, `Chitimacha.txt`), `utf8`);
+    const test = () => convert(Chitimacha);
     expect(test).not.to.throwError();
   });
 
-  it(`parses Nuuchahnulth`, () => {
-    const test = () => convert(data.Nuuchahnulth);
+  it.only(`parses Nuuchahnulth`, async () => {
+    const Nuuchahnulth = await readFile(path.join(currentDir, `Nuuchahnulth.txt`), `utf8`);
+
+    try {
+      convert(Nuuchahnulth);
+    } catch (e) {
+      console.error(e);
+    }
+
+
+    const test = () => convert(Nuuchahnulth);
     expect(test).not.to.throwError();
   });
 
