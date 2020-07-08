eadb149227defbec94df83a1aa1cdc6f77ca6f18
Danny Hieber <dwhieb@gmail.com>
2020-07-07 21:18:43 -0500

    rip out everything

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,53 @@
+/* eslint-disable
+  sort-keys,
+*/
+
+import { isString }    from './utilities/index.mjs';
+import parseHeader     from './parseHeader.mjs';
+import parseUtterances from './parseUtterances.mjs';
+
+const defaultCodes = {
+  sp:   `sp`,
+  trs:  `trs`,
+  txn:  `txn`,
+  w:    `w`,
+  phon: `phon`,
+  m:    `m`,
+  gl:   `gl`,
+  wlt:  `wlt`,
+  lit:  `lit`,
+  tln:  `tln`,
+  s:    `s`,
+  n:    `n`,
+};
+
 /**
- * Use this file when compiling with webpack + Babel and the "useBuiltIns" option.
- * This adds the required polyfills to the entry point of your library.
+ * Converts a scription-format text to DLx format
+ * @param  {String} scription The text of the Scription file to parse
+ * @param  {Object} options   An options hash
+ * @return {Object}           Returns a plain JavaScript object formatted according to the DLx Text format
  */
+export default function scription2dlx(scription = ``, {
+  codes: customCodes = {},
+  parser,
+  utteranceMetadata = false,
+} = {}) {
 
-import 'core-js';
+  const isEmpty = scription.trim() === ``;
 
-export { default } from './scription2dlx.mjs';
+  if (isEmpty) return {};
+
+  if (!(customCodes instanceof Object && Object.values(customCodes).every(isString))) {
+    const e = new TypeError(`The "codes" option must be an Object whose values are Strings.`);
+    e.name  = `InvalidCodesHashError`;
+    throw e;
+  }
+
+  const codes = Object.assign({}, defaultCodes, customCodes || {});
+
+  const header     = parseHeader(scription, parser);
+  const utterances = parseUtterances(scription, codes, { utteranceMetadata });
+
+  return { ...header, utterances };
+
+}
