8cd2d47d765e48b3f69d9b6ff9d6df5b23bc2506
Danny Hieber <dwhieb@gmail.com>
2020-07-08 20:22:06 -0500

    getCode

--- src/parseUtterance/index.js
+++ src/parseUtterance/index.js
@@ -2,7 +2,7 @@
   max-statements,
 */
 
-import { newlineRegExp } from '../utilities/regexp/index.js';
+import { mergeTranscriptions } from '../utilities/index.js';
 
 import parseLiteral       from './parseLiteral.js';
 import parseMetadata      from './parseMetadata.js';
@@ -16,50 +16,19 @@ import parseTranscription from './parseTranscription.js';
 import parseTranslation   from './parseTranslation.js';
 import parseWords         from './parseWords.js';
 
-import {
-  getCode,
-  getSchema,
-  mergeTranscriptions,
-} from '../utilities/index.js';
-
-/**
- * A regular expression to match the line data (excludes the leading backslash code)
- * @type {RegExp}
- */
-const lineDataRegExp = /^\\(?:(?:\w|-)+)(?<lineData>.*)$/u;
-
-/**
- * Create a lines hash from an array of strings and an array of line codes
- * @param  {Array}  lines
- * @param  {Array}  schema
- * @return {Object}
- */
-function createLinesHash(lines, schema) {
-  return lines.reduce((hash, line, i) => {
-
-    const code  = schema[i] || `n-${i}`; // treat extra lines as notes
-    const match = line.match(lineDataRegExp);
-    const data  = (match ? match.groups.lineData : line).trim();
-
-    hash[code] = data; // eslint-disable-line no-param-reassign
-
-    return hash;
-
-  }, {});
-}
-
 /**
  * Parses an individual utterance as a string and returns it as a DLx Utterance object
- * @param  {String} utteranceString The utterance string to parse
- * @param  {Array}  schema          An interlinear gloss schema, as an array of backslash codes (without leading slashes)
- * @param  {Object} codesHash       The line codes to use for each line type
- * @param  {Object} [options]       An options hash
- * @return {Object}                 Returns a DLx Utterance object, or null if there is no data
+ * @param  {String} rawLines  An array of utterance lines to parse
+ * @param  {Array}  schema    An interlinear gloss schema, as an array of backslash codes (without leading slashes)
+ * @param  {Object} codesHash The line codes to use for each line type
+ * @param  {Object} [options] An options hash
+ * @return {Object}           Returns a DLx Utterance object, or null if there is no data
  */
-export default function parseUtterance(utteranceString, schema, codesHash, { utteranceMetadata }) {
+export default function parseUtterance(rawLines, schema, codesHash, { utteranceMetadata }) {
 
   try {
 
+    // TODO: Is this the best way to do this? Can you do this inline?
     const {
       lit,
       n,
@@ -71,33 +40,38 @@ export default function parseUtterance(utteranceString, schema, codesHash, { utt
       sp,
     } = codesHash;
 
-    const rawLines = utteranceString
-    .split(newlineRegExp)
-    .map(line => line.trim());
+    const lines = rawLines.reduce((hash, line, i) => {
 
-    const lines = rawLines.filter(line => !line.startsWith(`#`));
-    const codes = lines.map(getCode).filter(Boolean);
+      // A regular exprssion to match line data (excluding leading backslash code)
+      const lineDataRegExp = /^\\(?:(?:\w|-)+)(?<lineData>.*)$/u;
 
-    if (codes.length) schema = getSchema(utteranceString); // eslint-disable-line no-param-reassign
+      const code  = schema[i] || `n-${i}`; // treat extra lines as notes; index is removed later
+      const match = line.match(lineDataRegExp);
+      const data  = (match ? match.groups.lineData : line).trim();
 
-    const linesHash = createLinesHash(lines, schema);
-    const noData    = !Object.values(linesHash).every(Boolean);
+      hash[code] = data; // eslint-disable-line no-param-reassign
+
+      return hash;
+
+    }, {});
+
+    const noData = !Object.values(lines).every(Boolean);
 
     if (noData) return null;
 
     // Extract known utterance properties and populate the utterance
 
     const metadata    = parseMetadata(rawLines);
-    const speaker     = parseSpeaker(linesHash[sp]);
-    const transcript  = parseTranscript(trs, linesHash);
-    let transcription = parseTranscription(txn, linesHash);
-    const phonetic    = parsePhonetic(linesHash[phon]);
-    const literal     = parseLiteral(lit, linesHash);
-    const translation = parseTranslation(tln, linesHash) || ``;
-    const source      = parseSource(linesHash[s]);
-    const notes       = parseNotes(n, linesHash);
-    const words       = parseWords(codesHash, linesHash);
-    const misc        = parseMisc(codesHash, linesHash);
+    const speaker     = parseSpeaker(lines[sp]);
+    const transcript  = parseTranscript(trs, lines);
+    let transcription = parseTranscription(txn, lines);
+    const phonetic    = parsePhonetic(lines[phon]);
+    const literal     = parseLiteral(lit, lines);
+    const translation = parseTranslation(tln, lines) || ``;
+    const source      = parseSource(lines[s]);
+    const notes       = parseNotes(n, lines);
+    const words       = parseWords(codesHash, lines);
+    const misc        = parseMisc(codesHash, lines);
 
     if (!transcription) {
       const wordTranscriptions = words.map(({ transcription: t }) => t);
@@ -120,10 +94,7 @@ export default function parseUtterance(utteranceString, schema, codesHash, { utt
 
   } catch (e) {
 
-    const utteranceText = utteranceString
-    .split(newlineRegExp)
-    .map(str => str.trim())
-    .join(`\n`);
+    const utteranceText = rawLines.join(`\n`);
 
     e.name    = `ParseUtterranceError`;
     e.message = `${e.message}\n\nUtterance text:\n\n${utteranceText}`;
--- src/parseUtterances.js
+++ src/parseUtterances.js
@@ -1,16 +1,10 @@
 import { getSchema }  from './utilities/index.js';
 import parseUtterance from './parseUtterance/index.js';
 
-/**
- * Finds the text of the utterances portion of a scription text and returns it
- * @param  {String} text The scription text
- * @return {String}
- */
-function getUtterancesString(text) {
-  return text
-  .split(/---/gsu)
-  .map(part => part.trim())
-  .pop();
+function splitUtterance(rawUtterance) {
+  return rawUtterance
+  .split(/\r?\n/gu) // newline regexp
+  .map(line => line.trim());
 }
 
 /**
@@ -22,14 +16,32 @@ function getUtterancesString(text) {
  */
 export default function parseUtterances(scription, codes, options) {
 
-  const utterancesString = getUtterancesString(scription);
+  const utterancesString = scription
+  .split(/---/gsu)
+  .map(part => part.trim())
+  .pop();
 
   if (!utterancesString) return [];
 
   const blankLinesRegExp = /(?:[ \t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*\r?\n){2,}/gsu;
-  const utterances       = utterancesString.split(blankLinesRegExp);
-  const schema           = getSchema(utterances[0]);
-  const parse            = utteranceString => parseUtterance(utteranceString, schema, codes, options);
+
+  const utterances = utterancesString
+  .split(blankLinesRegExp)
+  .map(splitUtterance);
+
+  const textSchema = getSchema(utterances[0]);
+
+  const parse = utterance => {
+
+    // NOTE: This isn't a precise check, but this is fine because
+    // the only consequence is that getSchema() is called unnecessarily below
+    const hasOwnSchema = utterance.some(line => line.startsWith(`\\`));
+
+    const utteranceSchema = hasOwnSchema ? getSchema(utterance) : textSchema;
+
+    return parseUtterance(utterance, utteranceSchema, codes, options);
+
+  };
 
   return utterances
   .map(parse)
