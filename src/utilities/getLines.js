b644069b9cb0a98f37c2a785f514fb2f2792c02e
Danny Hieber <dwhieb@gmail.com>
2020-07-09 00:29:16 -0500

    REFACTOR: getLines.js

--- src/utilities/getLines.js
+++ src/utilities/getLines.js
@@ -1,17 +1,17 @@
 import getLineType from './getLineType.js';
 
 /**
- * Extracts the provided line types from a lines hash, and returns them in a new object
+ * Extracts all lines of the provided types from a lines hash, and returns them in a new object
  * @param  {Array}  lines An array of line types to get
  * @return {Object}
  */
 export default function getLines(types, lines) {
 
-  const wordLineEntries = Object.entries(lines)
+  const entries = Object.entries(lines)
   .filter(([code]) => types.includes(getLineType(code)));
 
-  if (!wordLineEntries.length) return null;
+  if (!entries.length) return null;
 
-  return Object.fromEntries(wordLineEntries);
+  return Object.fromEntries(entries);
 
 }
